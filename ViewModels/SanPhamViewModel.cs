6fe34a22e679fe87256e78541101904802bb25c6
danhnee <khabu2k5@gmail.com>
2026-02-06 21:21:24 +0700

    Add files via upload

---
+++ ViewModels/SanPhamViewModel.cs
@@ -0,0 +1,11 @@
+namespace WebAspNet.ViewModels
+{
+    public class SanPhamViewModel
+    {
+        public string TenSp { get; set; }
+
+        public decimal GiaTien { get; set; }
+
+        public string AnhMoTa { get; set; }
+    }
+}
---
+++ ViewModels/StudentViewModel.cs
@@ -0,0 +1,12 @@
+namespace myMvcApp.ViewModels;
+
+public class StudentViewModel
+{
+    public int MaSinhVien { get; set; }
+
+    public string? TenSinhVien { get; set; }
+
+    public double DiemTrungBinh { get; set; }
+
+    public string? ChuyenNganh { get; set; }
+}
